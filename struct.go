37352dfa0efeb1379075d6b35d9811b54c9f85b1
Cristian Riano <cristiancarm11@gmail.com>
2021-02-25 18:51:12 +0100

    Struct examples

---
+++ struct.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+type person struct {
+	name string
+	age int
+}
+
+func main() {
+	p := person{name: "Juan", age: 23}
+	fmt.Println(p)
+
+	p.age = 25
+	fmt.Println(p.age)
+}
