3f954423b8bbb5e5a46de7713dfb7dfee5f82990
Cristian Riano <cristiancarm11@gmail.com>
2021-03-03 17:59:17 +0100

    Interfaces examples

--- main.go
+++ main.go
@@ -3,17 +3,16 @@ package main
 import (
 	"fmt"
 	"runtime"
-
-	"github.com/cristianriano/go-playground/tour"
 )
 
 func main() {
 	fmt.Println("Running on: ", runtime.GOOS)
 	// fmt.Println(tour.WordCount("My example"))
 
-	f := tour.Fibonacci()
-	for i := 0; i < 10; i++ {
-		fmt.Println(f())
-	}
+	// f := tour.Fibonacci()
+	// for i := 0; i < 10; i++ {
+	// 	fmt.Println(f())
+	// }
 	// workerPoolWithCache()
+	emptyInterfaceExample()
 }
--- struct.go
+++ struct.go
@@ -7,10 +7,56 @@ type person struct {
 	age int
 }
 
+func (p person) introduce() {
+	fmt.Println("My name is", p.name, "and I'm", p.age, "years old")
+}
+
+type people interface {
+	introduce()
+}
+
 func structExample() {
 	p := person{name: "Juan", age: 23}
 	fmt.Println(p)
 
 	p.age = 25
 	fmt.Println(p.age)
+}
+
+func interfaceExample() {
+	var p people
+	p = person{name: "Cristian", age: 27}
+	p.introduce()
+}
+
+// Use empty interface to hande any kind of value like Println
+func emptyInterfaceExample() {
+	var i interface{}
+	describe(i)
+
+	i = 42
+	describe(i)
+
+	i = "hello"
+	describe(i)
+}
+
+func typeAssertionExample() {
+	var i interface{} = "hello"
+
+	s := i.(string)
+	fmt.Println(s)
+
+	s, ok := i.(string)
+	fmt.Println(s, ok)
+
+	f, ok := i.(float64) // 0, false
+	fmt.Println(f, ok)
+
+	// f = i.(float64) // panic a.k.a run time error
+	// fmt.Println(f)
+}
+
+func describe(i interface{}) {
+	fmt.Printf("(%v, %T)\n", i, i)
 }
