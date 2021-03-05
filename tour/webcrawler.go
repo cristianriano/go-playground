ee5cef477aef110a468a0072314e034b849f4f6c
Cristian Riano <cristiancarm11@gmail.com>
2021-03-05 11:52:35 +0100

    Add WebCrawler with rate limiting

--- tour/webcrawler.go
+++ tour/webcrawler.go
@@ -3,7 +3,27 @@ package tour
 import (
 	"fmt"
 	"sync"
+	"time"
 )
+
+// RunWebCrawler runs the crawler example
+func RunWebCrawler() {
+	Crawl("https://golang.org/", 4, fetcher)
+}
+
+var wg sync.WaitGroup
+
+// Crawl uses fetcher to recursively crawl pages starting with url, to a maximum of depth.
+func Crawl(url string, depth int, fetcher Fetcher) {
+	wg.Add(1)
+	// crawlRecursive(url, depth, fetcher)
+	// crawlRecursiveUnique(url, depth, fetcher, &safeMap{cache: make(map[string] bool)})
+
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+	crawlRecursiveTimed(url, depth, fetcher, ticker)
+	wg.Wait()
+}
 
 // Fetcher implement an abstraction for web crawling
 type Fetcher interface {
@@ -16,8 +36,52 @@ type safeMap struct {
 	mu sync.RWMutex
 }
 
-// Crawl uses fetcher to recursively crawl pages starting with url, to a maximum of depth.
-func Crawl(url string, depth int, fetcher Fetcher, sm *safeMap) {
+func crawlRecursiveTimed(url string, depth int, fetcher Fetcher, ticker *time.Ticker) {
+	defer wg.Done()
+
+	if depth <= 0 {
+		return
+	}
+
+	<- ticker.C
+	body, urls, err := fetcher.Fetch(url)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("found: %s %q\n", url, body)
+	for _, u := range urls {
+		wg.Add(1)
+		go crawlRecursiveTimed(u, depth-1, fetcher, ticker)
+	}
+
+	return
+}
+
+func crawlRecursive(url string, depth int, fetcher Fetcher) {
+	defer wg.Done()
+
+	if depth <= 0 {
+		return
+	}
+
+	body, urls, err := fetcher.Fetch(url)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("found: %s %q\n", url, body)
+	for _, u := range urls {
+		wg.Add(1)
+		go crawlRecursive(u, depth-1, fetcher)
+	}
+
+	return
+}
+
+func crawlRecursiveUnique(url string, depth int, fetcher Fetcher, sm *safeMap) {
 	defer func() {
 		wg.Done()
 		sm.add(url)
@@ -38,20 +102,10 @@ func Crawl(url string, depth int, fetcher Fetcher, sm *safeMap) {
 			continue
 		}
 		wg.Add(1)
-		go Crawl(u, depth-1, fetcher, sm)
+		go crawlRecursiveUnique(u, depth-1, fetcher, sm)
 	}
 
 	return
-}
-
-var wg sync.WaitGroup
-
-//
-func RunWebCrawler() {
-	wg.Add(1)
-	sm := safeMap{cache: make(map[string]bool)}
-	Crawl("https://golang.org/", 4, fetcher, &sm)
-	wg.Wait()
 }
 
 func (sm *safeMap) alreadyVisited(url string) bool {
